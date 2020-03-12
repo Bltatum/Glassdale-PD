f528982ae42929c3cb0ffb56c52cbeab66b14dc4
Brennen <brennen@Users-MacBook-Pro-3.local>
2020-03-12 08:58:38 -0500

    got selector dropdown to work

--- scripts/criminals/criminalList.js
+++ scripts/criminals/criminalList.js
@@ -1,14 +1,41 @@
 import { useCriminals } from "./criminalDataProvider.js";
-import {criminal} from "./criminals.js"
+import { criminal } from "./criminals.js";
 
-const contentElement = document.querySelector(".criminalsContainer")
+const contentTarget = document.querySelector(".criminalsContainer")
+const eventHub = document.querySelector(".container")
 
-export const criminalList = () =>{
-    const criminalObjectArray = useCriminals()
+eventHub.addEventListener("crimeChosen", event => {
+    // Filter the list of criminal who committed the crime
 
-    for (const criminalObject of criminalObjectArray) {
-        const criminalHTMLrep = criminal(criminalObject)
-        contentElement.innerHTML += criminalHTMLrep
+    // Get the criminals
+    const criminals = useCriminals()
 
+    // Get the crime
+    const theCrimeThatWasChosen = event.detail.chosenCrime
+
+
+    // Look at all of the criminals and determine if each one is a vandal
+    const guiltyCriminals = criminals.filter(criminal => {
+        if (criminal.conviction === theCrimeThatWasChosen){ 
+            return true 
+        }
+            return false
     }
-}
+         )
+
+    // Clear inner HTML of the criminal list
+    contentTarget.innerHTML = ""
+
+    // Build it up again
+    for (const singleCriminal of guiltyCriminals) {
+        contentTarget.innerHTML += criminal(singleCriminal)
+    }
+})
+
+export const criminalList = () => {
+    const criminals = useCriminals()
+
+    for (const singleCriminal of criminals) {
+        contentTarget.innerHTML += criminal(singleCriminal)
+    }
+} 
